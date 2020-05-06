77fe1dee66ce2717733c380b94c3ff30e7c9c1d3
jreynoso <jreynoso@gmail.com>
2020-05-05 23:55:22 -0500

    BLUE: added request logging and endabled CORS

--- src/main/java/com/dispassionproject/gutterball/config/GutterballConfiguration.java
+++ src/main/java/com/dispassionproject/gutterball/config/GutterballConfiguration.java
@@ -4,6 +4,7 @@ import io.jsondb.JsonDBTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class GutterballConfiguration {
@@ -14,6 +15,16 @@ public class GutterballConfiguration {
     @Bean
     public JsonDBTemplate jsonDBTemplate() {
         return new JsonDBTemplate(jsonDbFilesDir, "com.dispassionproject.gutterball.api");
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        return filter;
     }
 
 }
--- src/main/java/com/dispassionproject/gutterball/controller/GameController.java
+++ src/main/java/com/dispassionproject/gutterball/controller/GameController.java
@@ -7,6 +7,7 @@ import com.dispassionproject.gutterball.api.Player;
 import com.dispassionproject.gutterball.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class GameController {
